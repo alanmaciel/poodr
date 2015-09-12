ef560d3ed3b8d928e428e1f8f60761a56a9d6aa5
Alan Maciel <alan@ubernetics.com>
2015-09-12 17:52:37 -0500

    Ratios calculated with a simple script

---
+++ bicycle.rb
@@ -0,0 +1,9 @@
+chainring = 52      #number of teeth
+cog = 11
+ratio = chainring / cog.to_f
+puts ratio # -> 4.727272
+
+chainring = 30
+cog = 27
+ratio = chainring / cog.to_f
+puts ratio # -> 1.111111

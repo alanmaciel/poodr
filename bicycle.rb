c5ce05a4e2cb5f0555c0bbaca106a5b230cc7ca7
Alan Maciel <alan@ubernetics.com>
2015-09-12 18:42:08 -0500

    Adding a Wheel Class, now Gear and Wheel Classes have a single responsibility you can change without consequence and reunse without duplication

--- bicycle.rb
+++ bicycle.rb
@@ -1,10 +1,10 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
-    @chainring = chainring
-    @cog = cog
-    @wheel = Wheel.new(rim, tire)
+  def initialize(chainring, cog, wheel=nil)
+    @chainring  = chainring
+    @cog        = cog
+    @wheel      = wheel
   end
 
   def ratio
@@ -22,8 +22,29 @@ class Gear
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim        = rim
+    @tire       = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+# -> 91.106186
+
+puts Gear.new(52, 11, @wheel).gear_inches
 # -> 137.090909
 
-puts Gear.new(52, 11, 24, 1.25).gear_inches
-# -> 125.272727
+puts Gear.new(52, 11).ratio
+# -> 4.727272
